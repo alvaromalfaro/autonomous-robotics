a016ab61ec73cf8441d3135c32635a38ae253a1c
alvaromalfaro <alvaromartinezalf@gmail.com>
2026-04-17 17:51:16 +0200

    Refactor controller initialization and improve report clarity in Homework 3

--- Homework3/advanced/homework3/homework3/controller.py
+++ Homework3/advanced/homework3/homework3/controller.py
@@ -18,43 +18,44 @@ class Controller(Node):
 
     def __init__(self):
         super().__init__('controller')
-        
-        self.radius = 2.0 
+
+        self.radius = 2.0
         self.speed = math.pi
-        
+
         # 0.- LAUNCH WILL AUTOMATICALLY EXECUTE A NEW SIMULATOR (nothing is needed)
-        
+
         # 1.- RESET, CLEAR SIMULATOR AND REMOVE INITIAL TURTLE
-        
-        self.reset()        
+
+        self.reset()
         self.clear()
         self.kill_turtle('turtle1')
-        
+
         # 2.- ADD A NEW TURTLE NAMED 'GUIDANCE', LOCATED AT INITIAL POSICION WITH EAST ORIENTATION (0 DEGREES)
         self.add_turtle("GUIDANCE", 5.5 - self.radius, 5.5 + self.radius, 0.0)
-        
+
         # 3.- SET COLOUR TO GREEN TO MARK THE GUIDANCE PATH
-        self.set_color('GUIDANCE', 0,255,0,10)        
+        self.set_color('GUIDANCE', 0, 255, 0, 10)
 
         # 4.- DRAW THE GUIDANCE PATH
         self.draw_path()
-            
-        # 5.- REMOVE GUIDANCE        
+
+        # 5.- REMOVE GUIDANCE
         self.kill_turtle('GUIDANCE')
-        
+
         # 6.- REMOVE TURTLE1 (FOR MULTIPLE TESTING) AND ADD IT INTO AN INITIAL RANDOM POSE
-        
-        self.add_turtle("turtle1", 5.5 - self.radius, 5.5 + self.radius + random.uniform(-0.5, 0.5), random.uniform(-0.2, 0.2))
-        
+
+        self.add_turtle("turtle1", 5.5 - self.radius, 5.5 + self.radius +
+                        random.uniform(-0.5, 0.5), random.uniform(-0.2, 0.2))
+
         # 7.- SET INITIAL COLOUR TO RED
-        self.set_color('turtle1', 255,0,0,2)        
-        
+        self.set_color('turtle1', 255, 0, 0, 2)
+
         # PD and Twiddle initialization
-        self.p = [171.2719, 38.5078] # tau_p, tau_d
-        self.dp = [0.05, 0.05] # initial tuning steps 
+        self.p = [0.9718, 1.0744]  # tau_p, tau_d
+        self.dp = [0.05, 0.05]  # initial tuning steps
         self.best_err = math.inf
-        self.tune_idx = 0 # 0: tau_p, 1: tau_d
-        self.tune_state = 0 # states for Twiddle
+        self.tune_idx = 0  # 0: tau_p, 1: tau_d
+        self.tune_state = 0  # states for Twiddle
 
         self.last_lap_time = 0.0
         self.lap_count = -1
@@ -62,21 +63,21 @@ class Controller(Node):
         self.lap_steps = 0
         self.prev_x = 5.5
         self.prev_cte = None
-        
+
         self.iteration = 0
         self.agg_error = 0
         self.last_exec_time = time.perf_counter()
-        
+
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
         self.subscription = self.create_subscription(
             Pose,
             '/turtle1/pose',
             self.listener_callback,
             10)
-        self.subscription      
-        
+        self.subscription
+
     ## METHODS TO BE USED ##
-        
+
     def kill_turtle(self, name):
         cli = self.create_client(Kill, '/kill')
         while not cli.wait_for_service(timeout_sec=1.0):
@@ -85,23 +86,23 @@ class Controller(Node):
         req.name = name
         future = cli.call_async(req)
         rclpy.spin_until_future_complete(self, future)
-        
+
     def reset(self):
         reset_cli = self.create_client(Empty, '/reset')
         while not reset_cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('...')
-        reset_req = Empty.Request()        
+        reset_req = Empty.Request()
         future = reset_cli.call_async(reset_req)
         rclpy.spin_until_future_complete(self, future)
-        
+
     def clear(self):
         clear_cli = self.create_client(Empty, '/clear')
         while not clear_cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('...')
-        clear_req = Empty.Request()        
+        clear_req = Empty.Request()
         future = clear_cli.call_async(clear_req)
         rclpy.spin_until_future_complete(self, future)
-        
+
     def set_color(self, turtlename, r, g, b, width):
         setpen_cli = self.create_client(SetPen, f'/{turtlename}/set_pen')
         while not setpen_cli.wait_for_service(timeout_sec=1.0):
@@ -109,53 +110,55 @@ class Controller(Node):
         setpen_req = SetPen.Request()
         setpen_req.r = r
         setpen_req.g = g
-        setpen_req.b = b        
+        setpen_req.b = b
         setpen_req.width = width
         future = setpen_cli.call_async(setpen_req)
         rclpy.spin_until_future_complete(self, future)
-        
+
     def add_turtle(self, name, posX, posY, theta):
         spawn_cli = self.create_client(Spawn, '/spawn')
         while not spawn_cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('...')
-        spawn_req = Spawn.Request()        
+        spawn_req = Spawn.Request()
         spawn_req.x = posX
         spawn_req.y = posY
         spawn_req.theta = theta
         spawn_req.name = name
-        future = spawn_cli.call_async(spawn_req)        
+        future = spawn_cli.call_async(spawn_req)
         rclpy.spin_until_future_complete(self, future)
-        
+
     def draw_path(self):
-        self.publisher_ref = self.create_publisher(Twist, '/GUIDANCE/cmd_vel', 10)
+        self.publisher_ref = self.create_publisher(
+            Twist, '/GUIDANCE/cmd_vel', 10)
         self.go_straight()
         self.go_curve(True)
         self.go_straight()
-        self.go_curve()    
-        
+        self.go_curve()
+
     def go_straight(self):
         msg = Twist()
         msg.linear.x = 2.0 * self.radius
         msg.linear.y = 0.0
-        self.publisher_ref.publish(msg)   
+        self.publisher_ref.publish(msg)
         time.sleep(2)
-        
+
     def go_curve(self, final_rotate=False):
         msg = Twist()
         msg.linear.y = 0.0
         msg.linear.z = 0.0
-        msg.angular.x = 0.0        
-        msg.angular.y = 0.0        
-        
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+
         msg.linear.x = self.speed
         msg.angular.z = -self.speed/self.radius
 
-        for _ in range(0,2):
+        for _ in range(0, 2):
             self.publisher_ref.publish(msg)
             time.sleep(1.5)
-            
+
         if final_rotate:
-            self._action_client = ActionClient(self, RotateAbsolute, '/GUIDANCE/rotate_absolute')
+            self._action_client = ActionClient(
+                self, RotateAbsolute, '/GUIDANCE/rotate_absolute')
             goal_msg = RotateAbsolute.Goal()
             goal_msg.theta = math.pi
             self._action_client.wait_for_server()
@@ -177,13 +180,13 @@ class Controller(Node):
         time_since = exec_time - self.last_exec_time
         if time_since < 0.1:
             return
-    
+
         self.last_exec_time = exec_time
         x, y, theta = msg.x, msg.y, msg.theta
-    
+
         new_msg = Twist()
         new_msg.linear.x = 0.2
-    
+
         # CTE computation
         if 3.5 <= x <= 7.5 and y > 5.5:
             new_cte = y - (5.5 + self.radius)
@@ -195,15 +198,15 @@ class Controller(Node):
         else:
             dist = math.sqrt((x - 3.5)**2 + (y - 5.5)**2)
             new_cte = dist - self.radius
-    
+
         if self.prev_cte is None:
             self.prev_cte = new_cte
-    
+
         # PD control law
         derivative = (new_cte - self.prev_cte) / time_since
         tau_p, tau_d = self.p[0], self.p[1]
         new_msg.angular.z = -tau_p * new_cte - tau_d * derivative
-    
+
         # Require 2 seconds between triggers and that we're in the upper half
         lap_gate_crossed = (
             y > 5.5
@@ -213,7 +216,7 @@ class Controller(Node):
         if lap_gate_crossed:
             self.last_lap_time = exec_time
             self.lap_count += 1
-    
+
             if self.lap_count == 1:
                 self.best_err = self.lap_err_sum / max(1, self.lap_steps)
                 self.get_logger().info(
@@ -222,7 +225,7 @@ class Controller(Node):
                 self.change_pen_color_async()
                 # First Twiddle perturbation
                 self.p[self.tune_idx] += self.dp[self.tune_idx]
-    
+
             elif self.lap_count > 1:
                 avg_err = self.lap_err_sum / max(1, self.lap_steps)
                 self.get_logger().info(
@@ -233,29 +236,28 @@ class Controller(Node):
                     f"New params: tau_p = {self.p[0]:.4f}, tau_d = {self.p[1]:.4f}"
                 )
                 self.change_pen_color_async()
-    
+
             self.lap_err_sum = 0.0
             self.lap_steps = 0
-    
+
         if self.lap_count >= 0:
             self.lap_err_sum += abs(new_cte)
             self.lap_steps += 1
-    
+
         self.prev_x = x
         self.prev_cte = new_cte
-    
+
         # Accumulation
         self.iteration += 1
         self.agg_error += new_cte
-    
+
         if self.iteration % 10 == 0:
             self.get_logger().info(
                 f"Average error {self.agg_error / float(self.iteration):.4f}"
             )
-    
+
         self.publisher_.publish(new_msg)
-    
-    
+
     def _twiddle_step(self, avg_err):
         """Twiddle state machine"""
         i = self.tune_idx
